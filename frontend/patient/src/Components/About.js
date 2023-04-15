e0ca2744b702115e7146c6e395058da2e6305042
Aryan6919 <113785954+Aryan6919@users.noreply.github.com>
2023-04-16 05:08:28 +0530

    Add files via upload

---
+++ frontend/patient/src/Components/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='px-44'>
+      <p className='text-4xl font-bold text-center underline mt-4'>About IKSHANA</p>
+      <br /><p className='text-xl'>A very important aspect of life to overcome is the limiting thought that hospitals/doctors/healthcare professionals have their vested interests in organ donation. ... One brain dead donor can save up-to eight lives, lives of people suffering from end stage organ failures.</p>
+      <br /><p className='text-xl'>Donation affects more than the donors and recipients. Hence we created Ikshana to encourage literacy and organ donation camps so that when one life ends another can be survived</p>
+      <p className='text-right text-5xl'>
+        <br />
+        <a target="_blank" href="https://github.com/Aryan6919/" className='hover:drop-shadow-md hover:text-purple'><i class="fa-brands fa-github"></i></a>&nbsp;&nbsp;&nbsp;
+        <a target="_blank" href="https://www.linkedin.com/in/aryan-arora-875884224/" className='hover:drop-shadow-md hover:text-metal'><i class="fa-brands fa-linkedin"></i> </a>&nbsp;&nbsp;
+      </p>
+    </div>
+  )
+}
+
+export default About
